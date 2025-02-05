09918b40645d8e780e4798cf074aaf083e948488
NoahLevyy <noah.levy@edu.devinci.fr>
2025-02-05 09:10:19 +0100

    NOAHHHH

--- Program.cs
+++ Program.cs
@@ -3,5 +3,6 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World! I am here man");
+        Console.WriteLine("I know, this is just the best !!");
         }
     }
--- README.md
+++
@@ -1,2 +0,0 @@
-# Liv-In-Paris
-A school project where we developp a meal-sharing service between residents of Paris
