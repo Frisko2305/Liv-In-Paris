f09f2d9a89931b2e4209b82a7513cf48e28530cb
Frisko2305 <niamor2305@gmail.com>
2025-02-28 22:59:44 +0100

    k

--- obj/Debug/net8.0-windows/Liv_In_Paris.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Liv_In_Paris.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Liv_In_Paris")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d67f2357cc83a4d2624e421359f2d6a323c0c2fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc2a4224ff87754ffaa17e2a5251596ad9c3bad8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Liv_In_Paris")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Liv_In_Paris")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
