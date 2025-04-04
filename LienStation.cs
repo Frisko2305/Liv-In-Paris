884dc73ae290084c972891b964ac1fe85bebecff
Maxian12 <maxian.lemarchand@gmail.com>
2025-04-04 08:50:16 +0200

    Add files via upload

---
+++ LienStation.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liv_In_Paris
+{
+    class LienStation
+    {
+
+        private int id;
+        private string nom;
+        private NoeudsStation id_precedent;
+        private NoeudsStation id_suivant;
+        private int poids;
+
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value ; }
+        }
+
+        public NoeudsStation Id_precedent
+        {
+            get { return Id_precedent; }
+            set { id_precedent = value; }
+        }
+
+        public NoeudsStation Id_suivant
+        {
+            get { return Id_suivant; }
+            set { id_suivant = value; }
+        }
+
+
+
+        public int Poids
+        {
+            get { return poids; }
+            set { poids = value; }
+        }
+
+
+        public LienStation(int id, string nom, NoeudsStation id_precedent,NoeudsStation id_suivant, int poids = 1)
+        {
+            this.id = id;
+            this.nom = nom;
+            this.id_precedent = id_precedent;
+            this.id_suivant = id_suivant;
+            this.poids = poids;
+        }
+
+
+    }
+}
---
+++ NoeudsStation.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liv_In_Paris
+{
+    class NoeudsStation
+    {
+        private int id;
+        private int libelle;
+        private string nom;
+        private double longitude;
+        private double latitude;
+        private string communenom;
+        private int codeInsee;
+        private int z;
+
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public int Libelle
+        {
+            get { return libelle; }
+            set { libelle = value; }
+        }
+
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+
+        public double Longitude
+        {
+            get { return longitude; }
+            set { longitude = value; }
+
+        }
+
+
+        public double Latitude
+        {
+            get { return latitude; }
+            set { latitude = value; }
+
+        }
+
+        public string Communenom
+        {
+            get { return communenom; }
+            set { communenom = value; }
+        }
+
+        public int CodeInsee
+        {
+            get { return codeInsee; }
+            set { codeInsee = value; }
+        }
+
+        public int Z
+        {
+            get { return z; }
+            set { z = value; }
+        }
+
+        public NoeudsStation(int son_id, int libelle, string nom, double longitude, double latitude,   string communenom, int codeInsee, int z)
+        {
+            this.id = son_id;
+            this.libelle = libelle;
+            this.nom = nom;
+            this.longitude = longitude;
+            this.latitude = latitude;
+            this.z = z;
+            this.communenom = communenom;
+            this.codeInsee = codeInsee;
+        }
+
+
+    }
+}
