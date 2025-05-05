a80eb60f4857314afe9068bd991ebb8a67e86f58
Frisko2305 <niamor2305@gmail.com>
2025-05-05 19:53:59 +0200

    DERNIER DEPOT (normalement)

--- obj/Debug/net8.0-windows/Liv_In_Paris.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Liv_In_Paris.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Liv_In_Paris")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17a7b2c07178730dd1c24e295c069acb09904d4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b487f55dfacbae16febefe88ca8807cf3ac5027c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Liv_In_Paris")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Liv_In_Paris")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
