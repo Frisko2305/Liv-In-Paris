be8be8e926ac5f1a67e1d5d08974d67c39251166
NoahLevyy <noah.levy@edu.devinci.fr>
2025-05-05 13:56:45 +0200

    Update Graphe.cs ///

--- Graphe/Graphe.cs
+++ Graphe/Graphe.cs
@@ -1,44 +1,72 @@
-﻿namespace Liv_In_Paris
+namespace Liv_In_Paris
 {
+    /// <summary>
+    /// Classe générique représentant un graphe composé de noeuds et de liens.
+    /// </summary>
+    /// <typeparam name="T">Type générique pour les identifiants des noeuds et des liens.</typeparam>
     internal class Graphe<T>
     {
-        
-
+        /// <summary>
+        /// Liste des noeuds dans le graphe.
+        /// </summary>
         private List<NoeudsStation<T>> noeuds;
+
+        /// <summary>
+        /// Liste des liens entre les noeuds dans le graphe.
+        /// </summary>
         private List<LienStation<T>> liens;
 
-        
-            
-
-        public List<LienStation<T>> Liens_Pte 
+        /// <summary>
+        /// Obtient ou définit la liste des liens dans le graphe.
+        /// </summary>
+        public List<LienStation<T>> Liens_Pte
         {
             get { return liens; }
             set { this.liens = value; }
         }
 
+        /// <summary>
+        /// Obtient ou définit la liste des noeuds dans le graphe.
+        /// </summary>
         public List<NoeudsStation<T>> Noeuds_Pte
         {
             get { return noeuds; }
-            set { this.noeuds =  value; }
+            set { this.noeuds = value; }
         }
 
+        /// <summary>
+        /// Initialise une nouvelle instance de la classe <see cref="Graphe{T}"/>.
+        /// </summary>
         public Graphe()
         {
             this.noeuds = new List<NoeudsStation<T>>();
             this.liens = new List<LienStation<T>>();
         }
 
-
-        public void AjouterNoeud( NoeudsStation<T> noeud)
+        /// <summary>
+        /// Ajoute un noeud au graphe.
+        /// </summary>
+        /// <param name="noeud">Le noeud à ajouter.</param>
+        public void AjouterNoeud(NoeudsStation<T> noeud)
         {
             this.noeuds.Add(noeud);
         }
 
-        public void AjouterLien (LienStation<T> lien)
+        /// <summary>
+        /// Ajoute un lien au graphe.
+        /// </summary>
+        /// <param name="lien">Le lien à ajouter.</param>
+        public void AjouterLien(LienStation<T> lien)
         {
             this.liens.Add(lien);
         }
 
+        /// <summary>
+        /// Recherche un noeud dans le graphe par son identifiant.
+        /// </summary>
+        /// <param name="id">L'identifiant du noeud à rechercher.</param>
+        /// <returns>Le noeud trouvé.</returns>
+        /// <exception cref="Exception">Lancée si le noeud n'existe pas.</exception>
         public NoeudsStation<T> RechercherNoeud(int id)
         {
             foreach (var noeud in this.noeuds)
@@ -51,11 +79,16 @@
             throw new Exception("Noeud inexistant");
         }
 
-
-
-       
-
-        /*public Dictionary<NoeudsStation<T>, int> BellmanFord(NoeudsStation<T> depart, List<NoeudsStation<T>> noeuds, List<LienStation<T>> liens)
+        /*
+        /// <summary>
+        /// Implémente l'algorithme de Bellman-Ford pour trouver le plus court chemin depuis un noeud de départ.
+        /// </summary>
+        /// <param name="depart">Le noeud de départ.</param>
+        /// <param name="noeuds">La liste des noeuds.</param>
+        /// <param name="liens">La liste des liens.</param>
+        /// <returns>Un dictionnaire des distances minimales depuis le noeud de départ.</returns>
+        /// <exception cref="InvalidOperationException">Lancée si le graphe contient un cycle de poids négatif.</exception>
+        public Dictionary<NoeudsStation<T>, int> BellmanFord(NoeudsStation<T> depart, List<NoeudsStation<T>> noeuds, List<LienStation<T>> liens)
         {
             var distances = new Dictionary<NoeudsStation<T>, int>();
             var precedents = new Dictionary<NoeudsStation<T>, NoeudsStation<T>>();
@@ -100,6 +133,12 @@
             return distances;
         }
 
+        /// <summary>
+        /// Implémente l'algorithme de Floyd-Warshall pour trouver les plus courts chemins entre toutes les paires de noeuds.
+        /// </summary>
+        /// <param name="noeuds">La liste des noeuds.</param>
+        /// <param name="graphe">Le graphe contenant les liens.</param>
+        /// <returns>Une matrice des distances minimales entre les noeuds.</returns>
         public int[,] FloydWarshall(List<NoeudsStation<T>> noeuds, Graphe<T> graphe)
         {
             int n = noeuds.Count;
@@ -121,6 +160,7 @@
             }
 
             return distances;
-        }*/
+        }
+        */
     }
 }
